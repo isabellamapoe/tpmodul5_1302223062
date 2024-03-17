0cc39badf38d3dc6d3221e3416ebf65b83cf46d0
isabella <78943065+isabellamapoe@users.noreply.github.com>
2024-03-17 23:56:23 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223062/Program.cs
+++ tpmodul5_1302223062/Program.cs
@@ -1,5 +1,33 @@
 ﻿using System;
 
+<<<<<<< HEAD
+class DataGeneric<T>
+{
+    private T data;
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah : " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Masukkan data:");
+        string Data = Console.ReadLine();
+
+        DataGeneric<string> dataString = new DataGeneric<string>(Data);
+        dataString.PrintData();
+
+        
+=======
 class HaloGeneric
 {
     public void SapaUser<T>(T nama)
@@ -15,5 +43,6 @@ class HaloGeneric
         string nama = Console.ReadLine();
 
         halo.SapaUser(nama);
+>>>>>>> 008ef129674e32d6c1158169979bcc5612f7db2f
     }
 }
